155ae2138b5e07968cf678ada2c3bdd83aedcf53
PeterJarvis <peter.jarvis@derivco.co.za>
2023-11-01 17:50:01 +0200

    Array from

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+const Cell: React.FC = () => {
+  return <div>Cell</div>;
+};
+
+
 const App: React.FC = () => {
-  return <h1>Helloo, React!</h1>;
+  return (
+    <>
+      {
+         Array.from({length: 10}, (_, i) => {
+          return <Cell/>
+        })
+      }
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
