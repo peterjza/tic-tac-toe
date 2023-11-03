3e75823ab9c04e6d3bd2e478b57f241c1fecf066
PeterJarvis <peter.jarvis@derivco.co.za>
2023-11-03 16:43:44 +0200

    minor progress

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './app.scss';
 
 const root = createRoot(document.getElementById('root')!);
 
-const Cell: React.FC = () => {
-  return <div>Cell</div>;
+const Cell: React.FC<{ idx: number, currentPlayer: number }> = ({idx, currentPlayer}) => {
+
+  const handleClick = () => {
+    debugger
+  }
+
+  return <div onClick={handleClick}></div>;
 };
 
-
 const App: React.FC = () => {
+  const [currentPlayer, setCurrentPlayer] = useState(0)
+
   return (
-    <>
+    <div className='board'>
       {
-        Array.from({length: 10}, (_, i) => <Cell/>)
+        Array.from({length: 9}, (_, i) => <Cell idx={i} currentPlayer={currentPlayer}/>)
       }
-    </>
+    </div>
   );
 };
 
